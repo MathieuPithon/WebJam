10dfe06ae00d2a75f3672a893a7007b370c1032f
Mathieu <craybloge@gmail.com>
2025-11-16 13:09:37 +0900

    fixing favicon issue

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -64,11 +64,8 @@ def hello():
     <html>
     <head>
         <title>Page Hello</title>
-        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="icon" href="{{ url_for('static', filename='favicon/favicon.ico') }}" />
+        <link rel="apple-touch-icon" href="{{ url_for('static', filename='favicon/apple-touch-icon.png') }}" />
         <style>
             body {
                 font-family: Arial, sans-serif;
